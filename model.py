626562651154304e9ecb4886afea7a75ed05fc81
jiaruifang <jiaruifang@tencent.com>
2022-01-24 10:18:49 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,100 @@
+import torch
+from torch.utils.checkpoint import checkpoint
+from torch.utils.data import SequentialSampler
+from transformers import BertConfig
+from transformers.models.bert.modeling_bert import BertEmbeddings
+from ophooks import register_ophooks_recursively, MemTracerOpHook
+
+class Encoder(torch.nn.Module):
+    def __init__(self, hidden_dim, is_ckp=False):
+        super(Encoder, self).__init__()
+        self.linear1 = torch.nn.Sequential(
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+            torch.nn.Linear(hidden_dim, hidden_dim),
+        )
+
+        self.linear3 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.linear4 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.linear5 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.is_ckp = is_ckp
+
+    def forward(self, x):
+        h2 = self.linear1(x)
+        if self.is_ckp:
+            h3 = checkpoint(self.linear3, h2)
+        else:
+            h3 = self.linear3(h2)
+        h4 = self.linear4(h3)
+        h5 = self.linear5(h4)
+        return h5
+
+
+def get_data_loader(
+    batch_size,
+    total_samples,
+    hidden_dim,
+    device,
+    data_type=torch.float,
+    is_distrbuted=False,
+):
+    train_data = torch.randn(total_samples, hidden_dim, device=device, dtype=data_type)
+    train_label = torch.empty(total_samples, dtype=torch.long, device=device).random_(
+        hidden_dim
+    )
+    train_dataset = torch.utils.data.TensorDataset(train_data, train_label)
+    if is_distrbuted:
+        sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        sampler = SequentialSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=sampler
+    )
+    return train_loader
+
+
+def get_bert_data_loader(
+    batch_size, total_samples, sequence_length, device, is_distrbuted=False
+):
+    train_data = torch.randint(
+        low=0,
+        high=10,
+        size=(total_samples, sequence_length),
+        device=device,
+        dtype=torch.long,
+    )
+    train_label = torch.zeros(total_samples, dtype=torch.long, device=device)
+    train_dataset = torch.utils.data.TensorDataset(train_data, train_label)
+    if is_distrbuted:
+        sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        sampler = SequentialSampler(train_dataset)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=sampler
+    )
+    return train_loader
+
+
+class SimpleModel(torch.nn.Module):
+    def __init__(self, hidden_dim, seq_len, is_ckp=False, is_share_param=False):
+        super(SimpleModel, self).__init__()
+        config = BertConfig()
+        config.vocab_size = 25
+        config.max_position_embeddings = seq_len
+        config.hidden_size = hidden_dim
+        self.embeddings_1 = BertEmbeddings(config)
+
+        self._is_share_param = is_share_param
+        if is_share_param:
+            self.embeddings_2 = self.embeddings_1
+        else:
+            self.embeddings_2 = BertEmbeddings(config)
+        self.encoder = Encoder(hidden_dim, is_ckp)
+        self.cross_entropy_loss = torch.nn.CrossEntropyLoss()
+
+    def forward(self, x, y):
+        h1 = self.embeddings_1(x)
+        h2 = self.embeddings_2(x)
+        h3 = h1 + h2
+        h3 = self.encoder(h3)
+        return self.cross_entropy_loss(h3[:, 0], y)
---
+++ train.py
@@ -0,0 +1,73 @@
+import logging
+import torch
+
+from model import SimpleModel, get_bert_data_loader
+from ophooks import register_ophooks_recursively, MemTracerOpHook
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+BATCH_SIZE = 8
+HIDDEN_DIM = 4
+SEQ_LEN = 128
+
+
+def model_func():
+    return SimpleModel(
+        hidden_dim=HIDDEN_DIM, seq_len=SEQ_LEN, is_ckp=True, is_share_param=True
+    )
+
+
+LR = 5e-5
+BETAS = (0.9, 0.999)
+EPS = 1e-6
+WEIGHT_DECAY = 0
+
+config = {
+    # The same format as optimizer config of DeepSpeed
+    # https://www.deepspeed.ai/docs/config-json/#optimizer-parameters
+    "optimizer": {
+        "type": "Adam",
+        "params": {
+            "lr": LR,
+            "betas": BETAS,
+            "eps": EPS,
+            "weight_decay": WEIGHT_DECAY,
+            "use_hybrid_adam": True,
+        },
+    },
+    "fp16": {
+        "enabled": True,
+        "loss_scale": 0,
+        "initial_scale_power": 2 ** 3,
+        "loss_scale_window": 1000,
+        "hysteresis": 2,
+        "min_loss_scale": 1,
+    },
+    "default_chunk_size": 1024,
+    "use_fake_dist": False,
+    "use_cpu_embedding": False,
+}
+
+torch.manual_seed(0)
+model = model_func()
+
+register_ophooks_recursively(model, [MemTracerOpHook()])
+optim = torch.optim.Adam(
+    model.parameters(), lr=LR, betas=BETAS, eps=EPS, weight_decay=WEIGHT_DECAY
+)
+model.cuda()
+
+train_loader = get_bert_data_loader(BATCH_SIZE, 10000, 128, device, False)
+
+for epoch in range(3):
+    for i, batch in enumerate(train_loader):
+        optim.zero_grad()
+        input_ids, labels = batch
+        loss = model(input_ids, labels)
+        loss.backward()
+        optim.zero_grad()
+        optim.step()
+        print(i, loss.item())
+        if i == 10:
+            exit()
+
+model.eval()
