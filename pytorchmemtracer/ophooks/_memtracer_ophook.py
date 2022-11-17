b72e30b91b87fd85e7ff309c4988ec633f34bd2a
jiaruifang <fangjiarui123@gmail.com>
2022-11-17 15:17:09 +0800

    use SyncCudaMemoryMonitor

--- pytorchmemtracer/ophooks/_memtracer_ophook.py
+++ pytorchmemtracer/ophooks/_memtracer_ophook.py
@@ -1,82 +1,63 @@
 import torch
 from . import BaseOpHook
-from concurrent.futures import ThreadPoolExecutor
-from time import sleep, time
-import psutil
+from time import time
 import pickle
+from abc import abstractmethod
 
-def get_cuda_memory_used(device):
+class MemoryMonitor:
+    """Base class for all types of memory monitor.
+    All monitors should have a list called `time_stamps` and a list called `mem_stats`.
     """
-    Get the free memory info of device.
-    Notice that for CPU, this function will return 1/N of the total free memory,
-    where N is the world size.
-    """
-    ret = torch.cuda.memory_allocated()
-    # get the peak memory to report correct data, so reset the counter for the next call
-    if hasattr(torch.cuda, "reset_peak_memory_stats"):  # pytorch 1.4+
-        torch.cuda.reset_peak_memory_stats()
-    return ret
 
-
-class AsyncMemoryMonitor:
-    def __init__(self, power=10):
-        """
-        An Async Mem Monitor runing during computing.
-        Sampling GPU memory usage of the current GPU dev
-        at interval of 1/(10**power) sec.
-        """
-        self.keep_measuring = False
-        self.executor = ThreadPoolExecutor(max_workers=1)
-        self.monitor_thread = None
-        self.interval = 1 / (10 ** power)
+    def __init__(self):
         self.time_stamps = []
         self.mem_stats = []
 
-    def set_interval(self, power: int):
-        self.interval = 1 / (10 ** power)
+    def __len__(self):
+        return len(self.mem_stats)
 
-    def is_measuring(self):
-        return self.keep_measuring
-    
+    @abstractmethod
     def start(self):
-        self.keep_measuring = True
-        self.monitor_thread = self.executor.submit(self._measure_usage)
-        
-    def finish(self):
-        if self.keep_measuring is False:
-            return 0
-        self.keep_measuring = False
-        max_usage = self.monitor_thread.result()
-        self.monitor_thread = None
-        self.time_stamps.append(time())
-        self.mem_stats.append(max_usage)
-        return max_usage
+        pass
 
-    def _measure_usage(self):
-        max_usage = 0
-        dev = torch.device(f"cuda:{torch.cuda.current_device()}")
-        while self.keep_measuring:
-            max_usage = max(
-                max_usage,
-                get_cuda_memory_used(dev),
-            )
-            sleep(self.interval)
-        return max_usage
+    @abstractmethod
+    def finish(self):
+        pass
 
     def state_dict(self):
         return {
-            "time_stamps" : self.time_stamps,
-            "mem_stats" : self.mem_stats,
+            "time_stamps": self.time_stamps,
+            "mem_stats": self.mem_stats,
         }
-
+    
     def save(self, filename):
         with open(filename, "wb") as f:
             pickle.dump(self.state_dict(), f)
-    
+
+class SyncCudaMemoryMonitor(MemoryMonitor):
+    """
+    A synchronized cuda memory monitor.
+    It only record the maximum allocated cuda memory from start point to finish point.
+    """
+
+    def __init__(self, power: int = 10):
+        super().__init__()
+
+    def start(self):
+        torch.cuda.synchronize()
+        torch.cuda.reset_peak_memory_stats()
+
+    def finish(self):
+        torch.cuda.synchronize()
+        self.time_stamps.append(time())
+        max_usage = torch.cuda.max_memory_allocated()
+        self.mem_stats.append(max_usage)
+        return max_usage
+
 class MemTracerOpHook(BaseOpHook):
     def __init__(self):
         super().__init__()
-        self.async_mem_monitor = AsyncMemoryMonitor()
+        self.async_mem_monitor = SyncCudaMemoryMonitor()
 
     def pre_fwd_exec(self, module: torch.nn.Module, *args):
         if module.training:
